fe628d8ae8676425bf270752203f7b581c789715
Enuni5 <emilio.nuni@gmail.com>
2021-03-17 11:33:06 +0100

    reverseString tests added

--- top.js
+++ top.js
@@ -1,8 +1,18 @@
-function capitalize(string) {
-  let capString;
-  let trimmedString = string.trim();
-  capString = trimmedString.charAt(0).toUpperCase();
-  return capString.concat(trimmedString.slice(1).toLowerCase());
+// function capitalize(string) {
+//   let capString;
+//   let trimmedString = string.trim();
+//   capString = trimmedString.charAt(0).toUpperCase();
+//   return capString.concat(trimmedString.slice(1).toLowerCase());
+// }
+
+// module.exports = capitalize;
+
+function reverseString(string) {
+  let reversedString = '';
+  for (let i = 0; i <= string.length; i++) {
+    reversedString = reversedString.concat(string.charAt(string.length - i));
+  }
+  return reversedString;
 }
 
-module.exports = capitalize;
+module.exports = reverseString;
--- top.test.js
+++ top.test.js
@@ -1,18 +1,32 @@
 const { test, expect } = require('@jest/globals');
-const capitalize = require('./top');
+// const capitalize = require('./top');
 
-test('capitalize string', () => {
-  expect(capitalize('hola')).toBe('Hola');
+// test('capitalize string', () => {
+//   expect(capitalize('hola')).toBe('Hola');
+// });
+
+// test('there are spaces', () => {
+//   expect(capitalize('qué buen día hace')).toBe('Qué buen día hace');
+// });
+
+// test('only first letter is capitalized', () => {
+//   expect(capitalize('hOLa')).toBe('Hola');
+// });
+
+// test('there is a space at the start of string', () => {
+//   expect(capitalize(' hola')).toBe('Hola');
+// });
+
+const reverseString = require('./top');
+
+test('takes a string and reverses it', () => {
+  expect(reverseString('hola')).toBe('aloh');
 });
 
-test('there are spaces', () => {
-  expect(capitalize('qué buen día hace')).toBe('Qué buen día hace');
-});
-
-test('only first letter is capitalized', () => {
-  expect(capitalize('hOLa')).toBe('Hola');
-});
-
-test('there is a space at the start of string', () => {
-  expect(capitalize(' hola')).toBe('Hola');
+test("but it's a long string", () => {
+  expect(
+    reverseString(
+      'En un lugar de la mancha, de cuyo nombre no quiero acordarme...',
+    ),
+  ).toBe('...emradroca oreiuq on erbmon oyuc ed ,ahcnam al ed ragul nu nE');
 });
