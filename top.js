debee5ebf8b7dc2f5884686f2294ee7ad4ff5116
Enuni5 <emilio.nuni@gmail.com>
2021-03-17 12:34:48 +0100

    added error handling tests

--- top.js
+++ top.js
@@ -7,12 +7,34 @@
 
 // module.exports = capitalize;
 
-function reverseString(string) {
-  let reversedString = '';
-  for (let i = 0; i <= string.length; i++) {
-    reversedString = reversedString.concat(string.charAt(string.length - i));
+// function reverseString(string) {
+//   let reversedString = '';
+//   for (let i = 0; i <= string.length; i++) {
+//     reversedString = reversedString.concat(string.charAt(string.length - i));
+//   }
+//   return reversedString;
+// }
+
+// module.exports = reverseString;
+
+function calc(operation, ...args) {
+  if (args.some((arg) => typeof arg !== 'number')) {
+    throw new Error('Please, enter only integers');
   }
-  return reversedString;
+
+  switch (operation) {
+    case 'sum':
+      return args.reduce((prev, actual) => prev + actual);
+    case 'subtract':
+      return args.reduce((prev, actual) => prev - actual);
+    case 'multiply':
+      return args.reduce((prev, actual) => prev * actual);
+    case 'divide':
+      return args.reduce((prev, actual) => prev / actual);
+
+    default:
+      throw new Error('Not such operation');
+  }
 }
 
-module.exports = reverseString;
+module.exports = calc;
--- top.test.js
+++ top.test.js
@@ -17,16 +17,44 @@ const { test, expect } = require('@jest/globals');
 //   expect(capitalize(' hola')).toBe('Hola');
 // });
 
-const reverseString = require('./top');
+// const reverseString = require('./top');
 
-test('takes a string and reverses it', () => {
-  expect(reverseString('hola')).toBe('aloh');
+// test('takes a string and reverses it', () => {
+//   expect(reverseString('hola')).toBe('aloh');
+// });
+
+// test("but it's a long string", () => {
+//   expect(
+//     reverseString(
+//       'En un lugar de la mancha, de cuyo nombre no quiero acordarme...',
+//     ),
+//   ).toBe('...emradroca oreiuq on erbmon oyuc ed ,ahcnam al ed ragul nu nE');
+// });
+
+const calc = require('./top');
+
+test('sums', () => {
+  expect(calc('sum', 1, 2, 3, 4)).toBe(10);
 });
 
-test("but it's a long string", () => {
-  expect(
-    reverseString(
-      'En un lugar de la mancha, de cuyo nombre no quiero acordarme...',
-    ),
-  ).toBe('...emradroca oreiuq on erbmon oyuc ed ,ahcnam al ed ragul nu nE');
+test('subtract', () => {
+  expect(calc('subtract', 1, 2, 3, 4)).toBe(-8);
+});
+
+test('multiply', () => {
+  expect(calc('multiply', 1, 2, 3, 4)).toBe(24);
+});
+
+test('divide', () => {
+  expect(calc('divide', 1, 2, 3, 4)).toBeCloseTo(0.04166667);
+});
+
+test('error when there is a non-integer', () => {
+  expect(() => calc('sum', 'o', 0, 2, 'e')).toThrow();
+  expect(() => calc('divide')).toThrow();
+  expect(() => calc('multiply', 'r', '3', 4)).toThrow();
+});
+
+test('operation not found', () => {
+  expect(() => calc('whatever', 1, 2, 3, 4)).toThrow();
 });
